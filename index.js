8514437e9d5a223ce5b34e41bef78d5ea54056ae
nakajmg <nakajima.jmg@gmail.com>
2016-07-14 11:49:26 +0900

    🍺 first commit🍺

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function (str) {
+  var cssColors = require('css-color-list')().join('|');
+  var rgbRegex = require('./regex/rgb');
+  var rgbaRegex = require('./regex/rgba');
+  var hexRegex = require('./regex/hex');
+  var hslRegex = require('./regex/hsl');
+  var hslaRegex = require('./regex/hsla');
+  
+  var regex = new RegExp('[\\s\'":,]+(' +
+    rgbRegex.source + '|' +
+    rgbaRegex.source + '|' +
+    hexRegex.source + '|' +
+    hslRegex.source + '|' +
+    hslaRegex.source + '|' +
+    cssColors + ')+[\\s\'";,\\)]+'
+    , 'ig');
+  
+  var matched = str.match(regex);
+  return matched ? matched.map(function(match) {
+    return match.replace(regex, '$1');
+  }) : matched;
+};
---
+++ test/test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var detectCssColors = require('..');
+
+var testString = '$red: #F21; .btn--yellow { color: $red; background-color: green; color: rgba(0,0,0,1); color: rgb( 1 , 0, 0 ); color: hsl(120 , 50% , 50% ); color: hsla(120, 50% , 50% ,.3  ); background: linear-gradient(to bottom right, rgba(255, 0, 0 , 0.1)); background: linear-gradient(to bottom right, rgb(100,0,0)); text-shadow: 0 0 #000, 0 0 #fff; }';
+
+describe('detect-css-colors', function() {
+  it('detect css colors', function() {
+    assert.deepEqual(
+      detectCssColors(testString),
+      [ '#F21', 'green', 'rgba(0,0,0,1)', 'rgb( 1 , 0, 0 )', 'hsl(120 , 50% , 50% )', 'hsla(120, 50% , 50% ,.3  )', 'rgba(255, 0, 0 , 0.1)', 'rgb(100,0,0)', '#000', '#fff']
+    );
+  });
+});
